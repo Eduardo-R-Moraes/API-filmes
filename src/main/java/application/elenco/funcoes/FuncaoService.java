a92990169c458be9010189a4928e790b835ddda7
Eduardo <160164398+Eduardo-R-Moraes@users.noreply.github.com>
2025-09-09 16:48:25 +0000

    fix: importação desnecessária

--- src/main/java/application/elenco/funcoes/FuncaoService.java
+++ src/main/java/application/elenco/funcoes/FuncaoService.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import application.elenco.artistas.Artista;
-
 @Service
 public class FuncaoService {
     @Autowired
